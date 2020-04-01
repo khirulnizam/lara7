c4fec7928b33493915536b626a201e5eb9b11b9c
Khirulnizam Abd Rahman <khirulnizam@gmail.com>
2020-04-01 22:57:32 +0800

    Show Record Form Caption

--- resources/views/trainings/show.blade.php
+++ resources/views/trainings/show.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Insert New Record</div>
+                <div class="card-header">Display Complete Record</div>
 
                 <div class="card-body">
                     
